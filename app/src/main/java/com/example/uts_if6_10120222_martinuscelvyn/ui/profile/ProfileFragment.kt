bdae1316584ef650e0c2a9bea254c555002a6898
Martinus <celvynmartinus@gmail.com>
2023-06-05 20:27:23 +0700

    maps new

--- app/src/main/java/com/example/uts_if6_10120222_martinuscelvyn/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/uts_if6_10120222_martinuscelvyn/ui/profile/ProfileFragment.kt
@@ -1,12 +1,15 @@
 package com.example.uts_if6_10120222_martinuscelvyn.ui.profile
 
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
+import android.text.util.Linkify
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.uts_if6_10120222_martinuscelvyn.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -24,10 +27,25 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        val textViewMapsLink = rootView.findViewById<TextView>(R.id.text_maps)
+
+        textViewMapsLink.setOnClickListener {
+            val uri = "https://maps.app.goo.gl/MNRbtsMiDzQkxVAj9"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+            val linkText = "Kp.Cilebak RT.02 RW.02, Ds. Rancamanyar\nKab. Bandung, Jawa Barat"
+
+            textViewMapsLink.text = linkText
+            Linkify.addLinks(textViewMapsLink, Linkify.WEB_URLS)
+            textViewMapsLink.linksClickable = true
+            textViewMapsLink.setLinkTextColor(Color.BLUE)
+
+            startActivity(intent)
+        }
+
+        return rootView
     }
-
-
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -36,6 +54,7 @@ class ProfileFragment : Fragment() {
         val btn_ig = view.findViewById<View>(R.id.btn_ig)
         val btn_wa = view.findViewById<View>(R.id.btn_wa)
         val btn_email = view.findViewById<View>(R.id.btn_email)
+
 
         btn_ig.setOnClickListener {
             openSocialMedia("https://instagram.com/martinuscelvyn_s?igshid=MzNlNGNkZWQ4Mg==")
